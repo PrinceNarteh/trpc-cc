1d885f8892206f55c909404ec2862957aebbc3ac
Prince Narteh <prinarteh@gmail.com>
2022-07-02 22:06:11 +0000

    implements register

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -8,23 +8,26 @@ const Login = () => {
   return (
     <div className="min-h-screen bg-slate-200 flex items-center">
       <div className="shadow-md border w-screen bg-white p-10 space-y-4 max-w-xl mx-auto">
-        <h3 className="text-center text-3xl mb-10">Login</h3>
+        <h3 className="text-center text-slate-700 text-3xl mb-10">Login</h3>
         <form className="w-full space-y-8">
-          <div className="relative border-2">
+          <div className="relative z-0 w-full mb-6 group">
             <input
               type="email"
               id="email"
-              className="w-full p-2 bg-transparent"
+              className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-slate-500 focus:outline-none focus:ring-0 focus:border-slate-600 peer"
+              placeholder=" "
+              required
+              {...register("email", { required: true })}
             />
             <label
               htmlFor="email"
-              className="bg-white px-1 absolute left-2 -top-3.5 text-gray-700"
+              className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-slate-600 peer-focus:dark:text-slate-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
             >
               Email Address
             </label>
           </div>
           <button
-            className="border border-teal-500 py-2 px-10 cursor-pointer hover:bg-teal-500 hover:text-white"
+            className="border border-slate-500 py-2 px-10 cursor-pointer hover:bg-slate-500 hover:text-white"
             type="submit"
           >
             Login
--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,46 +1,58 @@
 import Link from "next/link";
 import React from "react";
 import { useForm } from "react-hook-form";
+import { CreateUserInput } from "schema/user.schema";
+import { trpc } from "utils/trpc";
 
 const Register = () => {
-  const { handleSubmit, register } = useForm();
+  const { handleSubmit, register } = useForm<CreateUserInput>();
+  const { mutate, error } = trpc.useMutation(["users.register"]);
+
+  const onSubmit = (values: CreateUserInput) => mutate(values);
 
   return (
     <div className="min-h-screen bg-slate-200 flex items-center">
       <div className="shadow-md border w-screen bg-white p-10 space-y-4 max-w-xl mx-auto">
         <h3 className="text-center text-3xl mb-10">Register</h3>
-        <form className="w-full space-y-8">
-          <div className="relative w-full border-2">
+        <form onSubmit={handleSubmit(() => {})} className="w-full space-y-8">
+          <div className="relative z-0 w-full mb-6 group">
             <input
               type="text"
               id="name"
-              className="w-full p-2 bg-transparent"
+              className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-slate-500 focus:outline-none focus:ring-0 focus:border-slate-600 peer"
+              placeholder=" "
+              required
+              {...register("name", { required: true })}
             />
             <label
               htmlFor="name"
-              className="bg-white px-1 absolute left-2 -top-3.5 text-gray-700"
+              className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-slate-600 peer-focus:dark:text-slate-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
             >
               Full Name
             </label>
           </div>
-          <div className="relative border-2">
+          <div className="relative z-0 w-full mb-6 group">
             <input
               type="email"
               id="email"
-              className="w-full p-2 bg-transparent"
+              className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-slate-500 focus:outline-none focus:ring-0 focus:border-slate-600 peer"
+              placeholder=" "
+              required
+              {...register("email", { required: true })}
             />
             <label
               htmlFor="email"
-              className="bg-white px-1 absolute left-2 -top-3.5 text-gray-700"
+              className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-slate-600 peer-focus:dark:text-slate-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
             >
               Email Address
             </label>
           </div>
-          <input
-            className="border border-teal-500 py-2 px-10 cursor-pointer hover:bg-teal-500 hover:text-white"
+          <button
+            className="border border-slate-500 py-2 px-10 cursor-pointer hover:bg-slate-500 hover:text-white"
             type="submit"
-            value="Register"
-          />
+          >
+            Register
+          </button>
         </form>
         <div>
           <p className="text-center text-gray-600">
